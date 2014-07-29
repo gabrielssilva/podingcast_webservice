8ea09d9765562291766d9f69547343465a78f9eb
Gabriel Silva <gabriel.ssilva@outlook.com>
2014-07-29 16:59:52 -0300

    Adding functions to get podcasts from database

--- database.py
+++ database.py
@@ -17,3 +17,28 @@ class Database:
 	def insert_podcast(self, podcast):
 		# Maybe some validation...
 		self.podcasts.insert(podcast)
+
+	def get_podcasts(self):
+		cursor = self.podcasts.find()
+		all_podcasts = self.cursor_to_array(cursor)
+		return all_podcasts
+
+	def get_podcast(self, name):
+		cursor = self.podcasts.find({"name": {"$regex": name}})
+		podcasts = self.cursor_to_array(cursor)
+		return podcasts
+
+	def cursor_to_array(self, cursor):
+		podcasts = []
+		for podcast in cursor:
+			clean_podcast = self.filter_dict(podcast)
+			podcasts.append(clean_podcast)
+		return podcasts
+
+	# Need to remove the _id item from the dict
+	def filter_dict(self, podcast):
+		new_podcast = {}
+		for key in podcast:
+			if key != "_id":
+				new_podcast[key] = podcast[key]
+		return new_podcast
