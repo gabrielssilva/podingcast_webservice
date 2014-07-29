704fa303b3dad151888e925a01754e3f381d1ae7
Gabriel Silva <gabriel.ssilva@outlook.com>
2014-07-28 22:45:52 -0300

    Adding option to save a new podcast using the webservice

--- podingcast_webservice.py
+++ podingcast_webservice.py
@@ -1,8 +1,10 @@
 import web
+from database import Database
 
 urls = (
     '/podcasts', 'get_podcasts',
-    '/podcasts/(.*)', 'get_podcast_by_name'
+    '/podcasts/(.*)', 'get_podcast_by_name',
+    '/save_podcast/(.*)', 'save_podcast'
 )
 
 class get_podcasts:
@@ -12,6 +14,18 @@ class get_podcasts:
 class get_podcast_by_name:
 	def GET(self, podcast_name):
 		return "Just testing (" + podcast_name + ")..."
+
+class save_podcast:
+	def GET(self, podcast_name):
+		self.new_podcast(podcast_name)
+		return "OK"
+
+	def new_podcast(self, podcast_name):
+		podcast = { 
+			"name": podcast_name 
+		}
+		with Database("localhost", 27017) as database:
+			database.insert_podcast(podcast)
 
 if __name__ == "__main__":
 	app = web.application(urls, globals())
