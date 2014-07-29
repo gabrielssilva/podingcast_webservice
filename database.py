d5cb5518254de636c8a34c38f9b4ce7aa038db2a
--global <gabriel.ssilva@outlook.com>
2014-07-28 21:59:01 -0300

    Adding behaviors to database class (open and close connection, and insert data)

--- database.py
+++ database.py
@@ -1,9 +1,14 @@
+from pymongo import MongoClient
+
 class Database:
-	def open_conection():
-		pass
+	def open_connection(self, host, port):
+		self.client = MongoClient(host, port)
+		self.db = self.client.podingcast
+		self.podcasts = self.db.podcasts
 
-	def close_conection():
-		pass
+	def close_connection(self):
+		self.client.close()
 
-	def insert_podcast(podcast):
-		pass
+	def insert_podcast(self, podcast):
+		# Maybe some validation...
+		self.podcasts.insert(podcast)
