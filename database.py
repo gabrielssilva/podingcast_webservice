6580b238cf9a6132036729601fdfae49a2461f42
Gabriel Silva <gabriel.ssilva@outlook.com>
2014-07-28 22:45:11 -0300

    Changing database class to open and close the connection using __enter__ and __exit__ methods

--- database.py
+++ database.py
@@ -1,12 +1,17 @@
 from pymongo import MongoClient
 
 class Database:
-	def open_connection(self, host, port):
-		self.client = MongoClient(host, port)
+	def __init__(self, host, port):
+		self.host = host
+		self.port = port
+
+	def __enter__(self):
+		self.client = MongoClient(self.host, self.port)
 		self.db = self.client.podingcast
 		self.podcasts = self.db.podcasts
+		return self
 
-	def close_connection(self):
+	def __exit__(self, type, value, traceback):
 		self.client.close()
 
 	def insert_podcast(self, podcast):
